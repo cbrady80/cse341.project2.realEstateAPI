0ba98a5e1dfcc478ae611a67401f2f4461726704
Cheryl Brady <cbrady80@byui.edu>
2023-05-16 16:07:41 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// Import the dependencies
+const express = require('express');
+const bodyParser = require('body-parser'); // helps us decode the body from an HTTP request
+const mongodb = require('./db/connection');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+// Instantiate an express object
+const app = express();
+
+// Save a port number
+const port = process.env.PORT || 5000;
+
+
+app
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+    .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers', 
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+        );
+        res.setHeader('Content-Type', 'application/json');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        next();
+    })
+    .use('/', require('./routes')); // Calls the routes to view the data
+
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        // Event Listener
+        app.listen(port);
+        // Log message
+        console.log(`Connected to DB and listening at port ${port}`);
+    }
+});
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+// Import Express package Router
+const express = require('express');
+const router = express.Router();
+
+// USE request for swagger
+router.use('/', require('../swagger'));
+// USE request for PROPERTIES
+router.use('/properties', require('./properties'));
+//USE request for TENANTS
+router.use('/tenants', require('./tenants'));
+router.use(
+    '/',
+    (docData = (req, res) => {
+      let docData = {
+        documentationURL: 'https://github.com/cbrady80/real-estate-api',
+      };
+      res.send(docData);
+    })
+  );
+
+// Export
+module.exports = router;
