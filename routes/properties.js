d1e0b21c56c1713e08df4b3b2d770af71613e090
Cheryl Brady <cbrady80@byui.edu>
2023-05-18 13:45:26 -0600

    Created route files

---
+++ routes/properties.js
@@ -0,0 +1,26 @@
+// When one of these specific routes is used, then we call the function in the controller/properties.js
+
+// Import Express package Router
+const express = require('express');
+const router = express.Router();
+// Import controller
+const contactsController = require('../controllers/properties');
+
+//Route for retreiving all properties
+router.get('/', propertiesController.getAllProperties);
+
+//Route for retreiving one single property by id
+router.get('/:id', propertiesController.getPropertyById);
+
+//Route for creating a new property - POST
+router.post('/', propertiesController.newProperty);
+
+//Route for updating an exsisting property - PUT
+router.put('/:id', propertiesController.updateProperty);
+
+//Route for deleting a property - DELETE
+router.delete('/:id', propertiesController.deleteProperty);
+
+
+// Exports
+module.exports = router;
