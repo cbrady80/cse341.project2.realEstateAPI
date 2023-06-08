71a6edd1dabf693c9614246ff49e58c7b3f0faf9
Cheryl Brady <cbrady80@byui.edu>
2023-06-08 17:09:05 -0600

    moved auth0 stuff to properties controller

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,32 @@
 const express = require('express');
 const router = express.Router();
 
-const { auth, requiresAuth } = require('express-openid-connect');
-const dotenv = require('dotenv');
-dotenv.config();
+// const { auth, requiresAuth } = require('express-openid-connect');
+// const dotenv = require('dotenv');
+// dotenv.config();
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.SESSION_SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
-};
+// const config = {
+//   authRequired: false,
+//   auth0Logout: true,
+//   secret: process.env.SESSION_SECRET,
+//   baseURL: process.env.BASE_URL,
+//   clientID: process.env.AUTH0_CLIENT_ID,
+//   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+// };
 
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-router.use(auth(config));
+// // auth router attaches /login, /logout, and /callback routes to the baseURL
+// router.use(auth(config));
 
-// req.isAuthenticated is provided from the auth router
-router.get('/checkLoginStatus', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+// // req.isAuthenticated is provided from the auth router
+// router.get('/checkLoginStatus', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
 
-// Example used to get profile info
-router.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
+// // Example used to get profile info
+// router.get('/profile', requiresAuth(), (req, res) => {
+//   res.send(JSON.stringify(req.oidc.user));
+// });
 
 
 // USE request for swagger
